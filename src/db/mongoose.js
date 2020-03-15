c5c20c25c51927a08987aa818a51a9da75da8cfd
Andrew Michitson <michitson@yahoo.com>
2020-03-15 09:59:51 +0000

    adding eb directories and buildspec.yml

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-const mongoConnectString = process.env.MONGODB_URL
+//const mongoConnectString = process.env.MONGODB_URL
+
+const mongoConnectString = `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PWD}@docdb-2020-03-14-20-41-35.cluster-cxzwyo0p9ajk.eu-west-2.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`
 
 mongoose.connect(mongoConnectString, {
     useNewUrlParser: true,
